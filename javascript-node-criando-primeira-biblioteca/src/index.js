c2360693c4864e282cc6caa9f83494556bb0b1e5
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-19 17:37:38 -0300

    javascript Node criando primeira biblioteca: módulo 05

--- javascript-node-criando-primeira-biblioteca/src/index.js
+++ javascript-node-criando-primeira-biblioteca/src/index.js
@@ -1,44 +1,28 @@
-const fs = require('fs');
-// FilSystem : interagir com sistema de arquivos (criar arquivos, alterar arquivos)
-// readFile -> leitura de arquivo
+export function contaPalavras(texto) {
+  const paragrafos = extraiParagrafos(texto)
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+  })
+  return contagem;
+}
 
-//const caminhoArquivo = require('../arquivos/texto-web.txt');
-const caminhoArquivo = process.argv;
-// só funciona com módulos JS ou JSON
-const link = caminhoArquivo[2];
-
-fs.readFile(link, 'utf-8', (erro, texto) => {
-    quebraEmParagrafos(texto);
-    //verificaPalavrasDuplicadas(texto);  
-});
-
-function quebraEmParagrafos(texto) {
-    const paragrafos = texto.toLowerCase().split('\n');
-    const contagem = paragrafos.flatMap(paragrafo => {
-        if (!paragrafo) return [];
-        return verificaPalavrasDuplicadas(paragrafo)
-    })
-    console.log(contagem);
+function extraiParagrafos(texto) {
+  return texto.toLowerCase().split('\n');
 }
 
 function limpaPalavras(palavra) {
-    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
 }
 
-// criar array de palavras
-// contar ocorrências 
-// objeto com resultado
 function verificaPalavrasDuplicadas(texto) {
-    const listaPalavras = texto.split(' ');
-    const resultado = {}
-
-    // objeto[propriedade] = valor;
-    listaPalavras.forEach(palavra => {
-        if (palavra.length >= 3) {
-            const palavraLimpa = limpaPalavras(palavra);
-            resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
-        }
-    })
-
-    return resultado;
+  const listaPalavras = texto.split(' ');
+  const resultado = {};
+  listaPalavras.forEach(palavra => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
+    }
+  })
+  return resultado;
 }
